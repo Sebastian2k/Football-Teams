7a7d09f9626db0510765f35d365b0176457983ac
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2026-01-30 11:00:31 +0100

    chore: add status echoes

--- create_and_run.sh
+++ create_and_run.sh
@@ -4,5 +4,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 python3 extract_games.py
+echo "Compiling FootballNetworkGraph.java..."
 javac -cp "lib/*:." src/FootballNetworkGraph.java
+echo "Starting app.."
 java -cp "lib/*:src" FootballNetworkGraph
